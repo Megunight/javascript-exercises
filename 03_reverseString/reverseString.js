8ea60c9289ab76e888f9b0539045a4557ed1c70a
Megunight <shengnaaaa@gmail.com>
2024-08-13 22:56:41 -0700

    completed exercises 1-7

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    const strArr = str.split("").reverse();
+    let reversed = "";
 
+    for (const ch of strArr) {
+        reversed = reversed.concat(ch);
+    }
+
+    return reversed;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const cel = (temp - 32) * (5/9);
+  return Math.round(cel * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const fah = temp * (9/5) + 32;
+  return Math.round(fah * 10) / 10;
 };
 
 // Do not edit below this line
